413e0042afcefa7f9e7f1e6de35116d0c00b316a
Gabriel Lantigua <143743234+glantig1@users.noreply.github.com>
2023-10-23 15:59:02 -0400

    - Added entry fragment - Refactored functionality of main activity into fragment - set main activity to use fragment (being transaction -> add -> commit) - implemented proper layout for the fragment and main activity, ensuring that it doesn't create the fragment twice.

---
+++ app/src/main/java/edu/temple/fragmentrefactor/EntryFragment.kt
@@ -0,0 +1,44 @@
+package edu.temple.fragmentrefactor
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [EntryFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+
+
+class EntryFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_entry, container, false).apply{
+            val displayTextView = findViewById<TextView>(R.id.displayTextView)
+            val nameEditText = findViewById<EditText>(R.id.nameEditText)
+            val changeButton = findViewById<Button>(R.id.changeButton).setOnClickListener {
+                val name = nameEditText.text
+                displayTextView.text = if (name.isNotBlank()) {
+                    "Hello, $name!"
+                } else {
+                    "Please enter your name"
+                }
+            }
+
+        }
+    }
+}
